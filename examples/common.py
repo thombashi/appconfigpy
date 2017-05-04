72e5788890ca27c2accea50df09418938b238bd3
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-05-04 23:40:01 +0900

    Update an example

--- examples/common.py
+++ examples/common.py
@@ -11,20 +11,18 @@ from __future__ import unicode_literals
 import appconfigpy
 
 
-CONFIG_NAME = "example"
-CONFIG_ITEM_LIST = [
-    appconfigpy.ConfigItem(
-        name="token",
-        initial_value=None,
-        prompt_text="API Token",
-        default_display_style=appconfigpy.DefaultDisplayStyle.PART_VISIBLE
-    ),
-    appconfigpy.ConfigItem(
-        name="path",
-        prompt_text="Path",
-        initial_value=".",
-    ),
-]
-
 app_config_manager = appconfigpy.ConfigManager(
-    config_name=CONFIG_NAME, config_item_list=CONFIG_ITEM_LIST)
+    config_name="example",
+    config_item_list=[
+        appconfigpy.ConfigItem(
+            name="token",
+            initial_value=None,
+            prompt_text="API Token",
+            default_display_style=appconfigpy.DefaultDisplayStyle.PART_VISIBLE
+        ),
+        appconfigpy.ConfigItem(
+            name="path",
+            prompt_text="Path",
+            initial_value=".",
+        ),
+    ])
