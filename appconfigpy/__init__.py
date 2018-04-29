934bb81fd57cdd8f20f1bdf93bef7ee51e97c3b0
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-29 20:40:34 +0900

    Apply isort

--- appconfigpy/__init__.py
+++ appconfigpy/__init__.py
@@ -4,16 +4,8 @@
 .. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
 """
 
-from __future__ import absolute_import
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-from ._config import (
-    DefaultDisplayStyle,
-    ConfigItem,
-    ConfigManager,
-)
+from ._config import ConfigItem, ConfigManager, DefaultDisplayStyle
 from ._const import NULL_VALUE
-from ._logger import (
-    set_logger,
-    set_log_level,
-)
+from ._logger import set_log_level, set_logger
