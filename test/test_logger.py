5ab9f3b5ac32a42ff0579f9b9091f615d993abd9
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-05-06 16:38:56 +0900

    Add tests for logger

---
+++ test/test_logger.py
@@ -0,0 +1,46 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import print_function, unicode_literals
+
+import pytest
+from appconfigpy import set_log_level, set_logger
+
+
+logbook = pytest.importorskip("logbook", minversion="0.12.3", reason="requries logbook>=0.12.3")
+
+import logbook  # isort:skip
+
+
+class Test_set_logger(object):
+    @pytest.mark.parametrize(["value"], [[True], [False]])
+    def test_smoke(self, value):
+        set_logger(value)
+
+
+class Test_set_log_level(object):
+    @pytest.mark.parametrize(
+        ["value"],
+        [
+            [logbook.CRITICAL],
+            [logbook.ERROR],
+            [logbook.WARNING],
+            [logbook.NOTICE],
+            [logbook.INFO],
+            [logbook.DEBUG],
+            [logbook.TRACE],
+            [logbook.NOTSET],
+        ],
+    )
+    def test_smoke(self, value):
+        set_log_level(value)
+
+    @pytest.mark.parametrize(
+        ["value", "expected"], [[None, LookupError], ["unexpected", LookupError]]
+    )
+    def test_exception(self, value, expected):
+        with pytest.raises(expected):
+            set_log_level(value)
